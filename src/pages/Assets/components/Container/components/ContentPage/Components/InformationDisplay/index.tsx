641735792b8eaabef6810c71b924b0ae0282f7d0
Guilherme Rocha <devguilhermerocha@gmail.com>
2024-08-10 21:48:11 -0300

    style: information section

--- src/pages/Assets/components/Container/components/ContentPage/Components/InformationDisplay/index.tsx
+++ src/pages/Assets/components/Container/components/ContentPage/Components/InformationDisplay/index.tsx
@@ -1,33 +1,17 @@
-
+import EquipmentType from "./components/EquipmentType";
+import SensorInfo from "./components/SensorInfo";
 import "./styles.css"
-import MyImage from '../../../../../../../../assets/img1.png'
+
 const InformationDisplay = () => {
     return (
-      <div className="info-display-content">
+      <section className="info-display-content">
         <h1>Teste</h1>
         <div style={{width:"100%", height:"1px", backgroundColor:"var(--gray-border-color)"}}/>
-        <div className="info-content">
-          <div style={{display:"flex", alignItems:"center", justifyContent:"space-around", height:"100%"}}>
-            <div style={{width:"336px", height:"226px", border:"1px solid var(--gray-border-color)"}}>
-              <img src={MyImage}/>
-            </div>
-              <div>
-                <div>
-                  <h4>Tipo de equipamento</h4>
-                  <h4>Motor Eletrico "(Trifasico)"</h4>
-                </div>
-                <div style={{width:"100%", height:"1px", backgroundColor:"var(--gray-border-color)"}}/>
-                <div>
-                  <h4>Responsaveis</h4>
-                  <h4>E  Eletrica</h4>
-                </div>
-              </div>
+          <div className="info-content">
+              <EquipmentType/>
+              <SensorInfo/>
           </div>
-          
-          <div>
-          </div>
-        </div>
-      </div>
+      </section>
     );
  
   };
