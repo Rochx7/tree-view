ee73b25dc5e2f6973d97dc20d1e1644e7520aa33
Guilherme Rocha <devguilhermerocha@gmail.com>
2024-08-12 08:05:30 -0300

    fix: nomenclature

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,23 +1,25 @@
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { apexUnit, jaguarUnit, tobiasUnit } from '../mocks';
-import { Asset, Location, TreeNode, TreeView } from '../types';
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { apexUnit, jaguarUnit, tobiasUnit } from "../mocks";
+import { Asset, Location, TreeNode, TreeView } from "../types";
 
 const useFormattedData = () => {
   const [formattedData, setFormattedData] = useState<TreeView | []>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [searchParams] = useSearchParams();
-  const unit = searchParams.get("unit") || "tobias-unit"
+  const unit = searchParams.get("unit") || "tobias-unit";
 
   const hashUnit = {
     "apex-unit": apexUnit,
     "jaguar-unit": jaguarUnit,
-    "tobias-unit": tobiasUnit
-  }
+    "tobias-unit": tobiasUnit,
+  };
 
-  const formatArray = (arr: Array<Location | Asset>): (Location | Asset)[] => {
+  const formatHierarchy = (
+    arr: Array<Location | Asset>
+  ): (Location | Asset)[] => {
     const format: { [key: string]: Location | Asset } = {};
-    const result: (Location | Asset)[]  = [];
+    const result: (Location | Asset)[] = [];
 
     arr.forEach((item) => {
       format[item.id] = { ...item, children: [] };
@@ -37,53 +39,53 @@ const useFormattedData = () => {
     return result;
   };
 
-  useEffect(() => {
-    setIsLoading(true);
-
-    const addAssetsToLocation = (location: Location, assets: Asset[]) => {
-      if (!location.hasOwnProperty('sensorType')) {
-        location.isLocation = true;
+  const addAssetsToLocation = (location: Location, assets: Asset[]) => {
+    if (!location.hasOwnProperty("sensorType")) {
+      location.isLocation = true;
+    }
+    assets.forEach((asset) => {
+      if (asset.locationId === location.id) {
+        location.children.push(asset);
       }
-      assets.forEach((asset) => {
-        if (asset.locationId === location.id) {
-          location.children.push(asset);
-        }
-      });
+    });
 
-      location.children.forEach((child) => {
-        addAssetsToLocation(child, assets);
-      });
-    };
+    location.children.forEach((child) => {
+      addAssetsToLocation(child, assets);
+    });
+  };
 
-    const formatData = () => {
-      const assetsFormatado = formatArray(hashUnit[unit].assets);
-      const locationFormatado = formatArray(hashUnit[unit].locations);
-      const hashLocation = {};
+  const formatData = () => {
+    const assetsFormatado = formatHierarchy(hashUnit[unit].assets);
+    const locationFormatado = formatHierarchy(hashUnit[unit].locations);
+    const hashLocation = {};
 
-      locationFormatado.forEach((location) => {
-        hashLocation[location.id] = location;
-      });
+    locationFormatado.forEach((location) => {
+      hashLocation[location.id] = location;
+    });
 
-      const rootLocations = formatArray(hashUnit[unit].locations );
+    const rootLocations = formatHierarchy(hashUnit[unit].locations);
 
-      assetsFormatado.forEach((asset) => {
-        if (!asset.locationId) {
-          rootLocations.push(asset);
-        }
-      });
+    assetsFormatado.forEach((asset) => {
+      if (!asset.locationId) {
+        rootLocations.push(asset);
+      }
+    });
 
-      rootLocations.forEach((location) => {
-        addAssetsToLocation(location, assetsFormatado);
-      });
+    rootLocations.forEach((location) => {
+      addAssetsToLocation(location, assetsFormatado);
+    });
 
-      setFormattedData(rootLocations);
-    };
-    formatData(); 
-    setIsLoading(false);
+    setFormattedData(rootLocations);
+  };
 
+  useEffect(() => {
+    formatData();
   }, [unit]);
 
-  const findComponent = (data: TreeView, component: string): TreeNode | undefined => {
+  const findComponent = (
+    data: TreeView,
+    component: string
+  ): TreeNode | undefined => {
     if (!component) return;
     for (const node of data) {
       if (node.id === component) {
@@ -91,10 +93,10 @@ const useFormattedData = () => {
       }
       const found = findComponent(node.children, component);
       if (found) {
-        return found; 
+        return found;
       }
     }
-    return undefined; 
+    return undefined;
   };
 
   return { formattedData, isLoading, findComponent };
